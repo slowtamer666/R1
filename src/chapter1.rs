c765b7dff8bfda66102bdbc3a3804c0c393406b8
slowtamer666 <holychicken5050@gmail.com>
2024-10-26 03:13:41 +0300

    Update практична робота 1.rs

--- src/chapter1.rs
+++ src/chapter1.rs
@@ -1,1 +1,28 @@
+use std::f64::consts::PI;
+use std::io;
+
+fn main() {
+    // Введення значення x
+    println!("Введіть значення x:");
+
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Не вдалося зчитати рядок");
+    
+    // Перетворення введеного значення в число
+    let x: f64 = input.trim().parse().expect("Введіть коректне число");
+
+    // Перевірка, щоб sin(x) не дорівнював нулю
+    if x % PI == 0.0 {
+        println!("Помилка: значення sin(x) не може дорівнювати нулю.");
+        return;
+    }
+
+    // Обчислення y
+    let y = (1.0 / (x.powi(2) + 2.0).tan() + (x.powi(2) - 8.0).exp()) / x.sin();
+
+    // Виведення результату
+    println!("Значення y: {:.5}", y);
+}
 
